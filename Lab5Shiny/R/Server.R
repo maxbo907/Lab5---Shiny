5699a8a28d8ec17af0536e5c6aa4e45a86002f90
Maxime <maxbo907@student.liu.se>
2015-09-30 14:50:00 +0200

    $län <- [,3]

--- Lab5Shiny/R/Server.R
+++ Lab5Shiny/R/Server.R
@@ -6,8 +6,8 @@ shinyServer(function(input, output){
         selectedData <- reactive({
                 party<-paste(input$party,"proc",sep=".")
                 lan=input$lan
-                data.frame(kommun=theData$KOMMUN[which(theData$LÄN==lan)],
-                           votes=theData[which(theData$LÄN==lan),which(names(theData)==party)])
+                data.frame(kommun=theData$KOMMUN[which(theData[,3]==lan)],
+                           votes=theData[which(theData[,3]==lan),which(names(theData)==party)])
         })
         output$main_plot<-renderPlot({
                 plot(selectedData(),
