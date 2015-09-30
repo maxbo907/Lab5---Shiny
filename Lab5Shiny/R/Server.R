93cca3171ef37197826dfc1bec12dd71a8c92dca
Kresch <niclas.lovsjo@me.com>
2015-09-30 15:28:36 +0200

    something

--- Lab5Shiny/R/Server.R
+++ Lab5Shiny/R/Server.R
@@ -1,14 +1,13 @@
 library(httr)
 library(xlsx)
 library(Lab5)
-library(XLConnect)
 library(shiny)
 shinyServer(function(input, output){
         selectedData <- reactive({
                 party<-paste(input$party,"proc",sep=".")
                 lan=input$lan
-                data.frame(kommun=theData$KOMMUN[which(theData$LÄN==lan)],
-                           votes=theData[which(theData$LÄN==lan),which(names(theData)==party)])
+                data.frame(kommun=theData$KOMMUN[which(theData$LAAN==lan)],
+                           votes=theData[which(theData$LAAN==lan),which(names(theData)==party)])
         })
         output$main_plot<-renderPlot({
                 plot(selectedData(),
