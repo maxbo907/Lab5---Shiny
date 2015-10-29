414bbb4f93826e417d7a674b97003ae000e6afcf
Maxime Bonneau <maxbo907@student.liu.se>
2015-10-29 10:59:47 +0100

    became service

---
+++ Lab5Shiny/R/service.R
@@ -0,0 +1,33 @@
+library(httr)
+library(xlsx)
+library(Lab5)
+library(shiny)
+library(roxygen2)
+
+#'
+#' @title shinyServer
+#' @name shinyServer
+#' @param function(input, output) : an anonymous function taking count of what
+#'        is chosen by the user and gives the corresponding output
+#' @description depending of the selected län and party, an histogram is
+#'        given with the score of this party in the different kommuner of
+#'        this län.
+#' @author Niclas Lovsjö & Maxime Bonneau
+#'
+#'
+shinyServer(function(input, output){
+  selectedData <- reactive({
+    party<-paste(input$party,"proc",sep=".")
+    lan=input$lan
+    data.frame(kommun=theData$KOMMUN[which(theData$LAAN==lan)],
+               votes=theData[which(theData$LAAN==lan),which(names(theData)==party)])
+  })
+  output$main_plot<-renderPlot({
+    plot(selectedData(),
+         main=paste(input$party,"in",input$lan),
+         ylab="% of votes",
+         xlab=paste("Kommuner i",input$lan),
+         type="p")
+  })
+  
+})
